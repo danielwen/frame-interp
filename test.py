0b3480818fbf837bf01b95598fface488d7c0176
Daniel Wen <danielwen@users.noreply.github.com>
2019-03-26 14:00:15 -0400

    testing code

--- test.py
+++ test.py
@@ -1,42 +1,25 @@
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
+import keras
+from data_generator import DataGenerator
+from model import VAE
 
-path1 = "challenge/JPEGImages/480p/ocean-birds/00000.jpg"
-path2 = "challenge/JPEGImages/480p/ocean-birds/00002.jpg"
+batch_size = 16
+image_size = 256
+latent_size = 512
+n_context = 2
 
-def imshow(im):
-    plt.figure()
-    plt.imshow(np.flip(np.round(255 * im).astype("int"), -1))
+train_data = DataGenerator("DAVIS_Train_Val", batch_size, image_size,
+    latent_size, n_context, test=True)
+val_data = DataGenerator("DAVIS_Dev", batch_size, image_size, latent_size,
+    n_context, test=True)
+test_data = DataGenerator("DAVIS_Challenge", batch_size, image_size, latent_size,
+    n_context, test=True)
 
-def get_target_indices(flow, h, w):
-    I, J = np.mgrid[:h, :w]
-    I = np.clip(I + flow[:, :, 0], 0, h - 1)
-    J = np.clip(J + flow[:, :, 1], 0, w - 1)
-    return I, J
+vae = VAE()
+vae.load("model.h5")
+train_loss = vae.model_test.evaluate_generator(train_data, steps=train_data.steps)
+val_loss = vae.model_test.evaluate_generator(val_data, steps=val_data.steps)
+test_loss = vae.model_test.evaluate_generator(test_data, steps=val_data.steps)
 
-def fetch(flow, target):
-    h, w, _ = target.shape
-    I, J = get_target_indices(flow, h, w)
-    return target[I, J, :]
-
-flow = np.load("flow.npy")
-frame1 = cv.imread(path1)
-frame2 = cv.imread(path2)
-frame1_float = frame1.astype("float64") / 255
-frame2_float = frame2.astype("float64") / 255
-
-flow_int = np.round(flow).astype("int")
-
-src2tgt = fetch(flow_int, frame2_float)
-
-imshow((frame1_float + frame2_float) / 2)
-h, w, _ = flow.shape
-y, x = np.mgrid[:h, :w][:, 0:h:16, 0:w:14]
-u = flow[0:h:16, 0:w:14, 0]
-v = flow[0:h:16, 0:w:14, 1]
-plt.quiver(x, y, u, -v)
-imshow(src2tgt)
-imshow((frame1_float + src2tgt) / 2)
-imshow((frame2_float + src2tgt) / 2)
-plt.show()
+print(train_loss)
+print(val_loss)
+print(test_loss)
